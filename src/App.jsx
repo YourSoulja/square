43a828f63bc6e60f14b17dfbbc5e5405f3471908
YourSoulja <157317101+YourSoulja@users.noreply.github.com>
2025-03-09 01:25:03 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='squareDiv'>
+      <div className='square'>
+        <p>Square</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
